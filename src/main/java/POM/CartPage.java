0f42e1b31ba4777ba428787588c02cbf58249fec
Axterix96 <xnicolaz96@gmail.com>
2023-11-08 11:43:44 -0600

    changes added to CartPage.java

--- src/main/java/POM/CartPage.java
+++ src/main/java/POM/CartPage.java
@@ -1,31 +1,45 @@
 package POM;
 
+import junit.framework.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-
-import java.util.ArrayList;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.util.List;
 
 public class CartPage extends BasePage{
 
-By table = By.xpath("//tbody//tr[@class='woocommerce-cart-form__cart-item cart_item']//td");
+By table = By.xpath("//tr[@class='woocommerce-cart-form__cart-item cart_item']//td");
 
-By increaseProductInput = By.xpath("//tbody//tr[@class='woocommerce-cart-form__cart-item cart_item']//input[@class='input-text qty text']//..//..//..//a[text()='Proton Rocket']");
+By loadingIcon = By.xpath("//div[@class='blockUI blockOverlay']");
 
+By cartMsg = By.xpath("//div[@class='woocommerce-message']");
+By checkoutBtn = By.xpath("//a[@class='checkout-button button alt wc-forward']");
 
-public void increaseProduct(String productName, int numberOfProducts)
-{
+public void increaseProduct(String productName, int numberOfProducts) throws InterruptedException {
     List<WebElement> rockets = driver.findElements(table);
 
     for (WebElement rocket : rockets) {
+        System.out.println(rocket.getText());
         if (rocket.getText().equals(productName)) {
             WebElement rocketFound = rocket.findElement(By.xpath("//a[text()='" + productName + "']//..//..//input[@class='input-text qty text']"));
-            for (int j = 0; j <= numberOfProducts; j++) {
-                rocketFound.sendKeys(Keys.UP);
-            }
+            rocketFound.clear();
+            rocketFound.sendKeys(String.valueOf(numberOfProducts)+Keys.ENTER);
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(loadingIcon));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(cartMsg));
+            String cartMessage = driver.findElement(cartMsg).getText();
+
+            Assert.assertEquals("Si se agrego el cambio de producto",cartMessage,"Cart updated.");
         }
     }
+}
+
+public void buyProductsWithoutCoupon()
+{
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    js.executeScript("window.scrollBy(0,350)", "");
+    driver.findElement(checkoutBtn).click();
 }
 
 
--- src/test/java/Tests/TestCase.java
+++ src/test/java/Tests/TestCase.java
@@ -9,12 +9,11 @@ public class TestCase extends BaseTest{
 PomManager pomManager = new PomManager();
 
 @Test
-        public void test1()
-{
+        public void test1() throws InterruptedException {
     pomManager.homePage.addProduct("Falcon 9");
     pomManager.homePage.addProduct("Proton Rocket");
     pomManager.homePage.clickCartBtn();
-    pomManager.cartPage.increaseProduct("Falcon 9",2);
+    pomManager.cartPage.increaseProduct("Falcon 9",4);
 }
 
 }
